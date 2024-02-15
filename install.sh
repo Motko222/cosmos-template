8ce3ba88ded07e25e56d7e8108797d4581fc9af9
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-15 14:05:25 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+curl -s https://get.nibiru.fi/@v1.0.0! | bash
+nibid version
+nibid init $MONIKER --chain-id=nibiru-testnet-1 --home $HOME/.nibid
+nibid keys add $KEY
+curl -s https://networks.testnet.nibiru.fi/$NETWORK/genesis > $HOME/.nibid/config/genesis.json
+sed -i 's|seeds =.*|seeds = "'$(curl -s https://networks.testnet.nibiru.fi/$NETWORK/seeds)'"|g' $HOME/.nibid/config/config.toml
+sed -i 's/minimum-gas-prices =.*/minimum-gas-prices = "0.025unibi"/g' $HOME/.nibid/config/app.toml
+
+config_file="$HOME/.nibid/config/config.toml"
+
+sed -i "s|enable =.*|enable = true|g" "$config_file"
+sed -i "s|rpc_servers =.*|rpc_servers = \"$(curl -s https://networks.testnet.nibiru.fi/$NETWORK/rpc_servers)\"|g" "$config_file"
+sed -i "s|trust_height =.*|trust_height = \"$(curl -s https://networks.testnet.nibiru.fi/$NETWORK/trust_height)\"|g" "$config_file"
+sed -i "s|trust_hash =.*|trust_hash = \"$(curl -s https://networks.testnet.nibiru.fi/$NETWORK/trust_hash)\"|g" "$config_file"
+
+#create service
+sudo tee /etc/systemd/system/nibi.service > /dev/null << EOF
+[Unit]
+Description=nibi node service
+After=network-online.target
+
+[Service]
+User=root
+ExecStart=/usr/local/bin/nibid start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=/root/.nibi"
+Environment="DAEMON_NAME=nibi"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable nibi.service
+
+echo "Installation done, service is not started. Please run it with start.sh."
