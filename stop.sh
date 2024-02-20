0c401ce35fa37056531b61e2019ac98891b35f38
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-20 22:16:00 +0100

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-source ~/scripts/nibiru/config/env
+
+FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$FOLDER/config/env
+
 sudo systemctl stop $BINARY.service
