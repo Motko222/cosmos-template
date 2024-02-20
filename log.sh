cb8a185388d9d776c953cfe98106ace9357b7320
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-20 22:14:52 +0100

    Update log.sh

--- log.sh
+++ log.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-source ~/scripts/nibiru/config/env
+FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$FOLDER/config/env
+
 sudo journalctl -u $BINARY.service -f --no-hostname -o cat
