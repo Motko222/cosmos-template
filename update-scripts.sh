3e5e9551f341dab8662d39c629deed0b59ebfb20
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-20 22:16:47 +0100

    Update update-scripts.sh

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,4 +1,8 @@
-cd ~/scripts/nibiru
+#!/bin/bash
+
+FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+
+cd ~/scripts/$FOLDER
 git stash push --include-untracked
 git pull
 chmod +x *.sh
