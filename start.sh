9ec660c8c903d1f84ac87ccc761fbb7cedad2e93
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-20 22:15:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-source ~/scripts/nibiru/config/env
+
+FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$FOLDER/config/env
+
 sudo systemctl restart $BINARY.service
 sudo journalctl -u $BINARY.service -f --no-hostname -o cat
