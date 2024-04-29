82ac5172e90da74092b333ce7152500215cf6a75
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-04-29 15:40:00 +0200

    Update update-scripts.sh

--- update-scripts.sh
+++ update-scripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 
-cd ~/scripts/$FOLDER
+cd ~/scripts/$folder
 git stash push --include-untracked
 git pull
 chmod +x *.sh
