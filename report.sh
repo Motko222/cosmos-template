268d2afea36c26eabf3e3f1bf5cb9a8ed6852391
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-04-29 15:38:51 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
-source ~/scripts/$FOLDER/config/env
+folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$folder/cfg
 
 json=$(curl -s localhost:$PORT/status | jq .result.sync_info)
 
