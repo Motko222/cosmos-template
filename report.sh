8e251b0beead096890a1b12f4855d6cccf04abfa
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-06 12:50:00 +0200

    Update report.sh

--- report.sh
+++ report.sh
@@ -3,7 +3,7 @@
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 source ~/scripts/$folder/cfg
 source ~/.bash_profile
-bucket=validators
+bucket=validator
 
 json=$(curl -s localhost:$PORT/status | jq .result.sync_info)
 
