9aa8b7e31e891af09dc6111357ec24c5aea93ea3
Motko <77466238+Motko222@users.noreply.github.com>
2024-05-16 21:22:26 +0200

    Update create-wallet.sh

--- create-wallet.sh
+++ create-wallet.sh
@@ -3,12 +3,7 @@
 folder=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
 source ~/scripts/$folder/cfg
 
-if [ -z $1 ]
-then
- read -p "Key name ? " key
-else
- key=$1
-fi
+[ -z $1 ] && read -p "Key name ? " key || key=$1
 
 $BINARY keys add $key
 $BINARY keys show $key --bech val
